1ebcc920d2e2788e30f3efe69a3004586cd2478a
Carlos ' Arjona <eduardo.arjona@hotmail.com>
2017-12-04 20:39:36 -0200

    criei a classe patrimonio

---
+++ WebAppPB2/Models/Patrimonio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppPB2.Models
+{
+    public class Patrimonio
+    {
+        public int Id { get; set; }
+
+
+    }
+}
