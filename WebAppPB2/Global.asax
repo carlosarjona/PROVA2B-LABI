b33a3c96e42ed20a908db9e0cd0576fb93ebf5ec
Carlos ' Arjona <eduardo.arjona@hotmail.com>
2017-12-04 20:32:09 -0200

    Add project files.

---
+++ WebAppPB2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppPB2.MvcApplication" Language="C#" %>
---
+++ WebAppPB2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppPB2.Startup))]
+namespace WebAppPB2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
