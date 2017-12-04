cb97bd11904e393e7bddf3153bed936bd64b84ac
Carlos ' Arjona <eduardo.arjona@hotmail.com>
2017-12-04 21:01:36 -0200

    criação das classes

--- WebAppPB2/Models/Patrimonio.cs
+++ WebAppPB2/Models/Patrimonio.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +9,17 @@ namespace WebAppPB2.Models
 {
     public class Patrimonio
     {
+        [Key]
+        [Column(Order = 1)]
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Campo obrigatorio")]
+        public string NumeroDeTombo { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatorio")]
+        public string Descricao { get; set; }
+
+        public Tipo Tipo { get; set; }
 
     }
 }
---
+++ WebAppPB2/Models/Tipo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAppPB2.Models
+{
+    public enum Tipo
+    {
+        [Display(Name = "Bens Duraveis")] BensDuraveis = 1,
+        [Display(Name = "Bens Nao Duraveis")] BensNaoDuraveis = 2
+
+    }
+}
